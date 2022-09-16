d390cc396f5377496c6a0dd7476967e3f8ddf150
rogue-tech <113648231+rogue-tech@users.noreply.github.com>
2022-09-15 18:25:12 -0700

    Update Classes: Assigning Values to Instance Fields.java

--- Classes: Assigning Values to Instance Fields.java
+++ Classes: Assigning Values to Instance Fields.java
@@ -11,3 +11,71 @@ public class Store {
   // main method
   public static void main(String[] args) {
     Store fractalPrinting = new Store("Fractals");
+
+    /* 
+    Output: Fractals
+  */  
+    
+    /* Classes: Multiple Fields */
+    
+public class Store {
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public Store(String product, int count, double price) {
+  String productType = product;
+  int inventoryCount = count;
+  double inventoryPrice = price;
+  s
+  
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    Store cookieShop = new Store("cookies", 12, 3.75);
+  }
+}
+    
+    
+    /* Review */
+    
+    
+public class Dog {
+  String breed;
+  boolean hasOwner;
+  int age;
+  
+  public Dog(String dogBreed, boolean dogOwned, int dogYears) {
+    System.out.println("Constructor invoked!");
+    breed = dogBreed;
+    hasOwner = dogOwned;
+    age = dogYears;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Main method started");
+    Dog fido = new Dog("poodle", false, 4);
+    Dog nunzio = new Dog("shiba inu", true, 12);
+    boolean isFidoOlder = fido.age > nunzio.age;
+    int totalDogYears = nunzio.age + fido.age;
+    System.out.println("Two dogs created: a " + fido.breed + " and a " + nunzio.breed);
+    System.out.println("The statement that fido is an older dog is: " + isFidoOlder);
+    System.out.println("The total age of the dogs is: " + totalDogYears);
+    System.out.println("Main method finished");
+  }
+}
+    
+    /* Output
+    
+    Output:
+
+Main method started
+Constructor invoked!
+Constructor invoked!
+Two dogs created: a poodle and a shiba inu
+The statement that fido is an older dog is: false
+The total age of the dogs is: 16
+Main method finished
+
+    */
